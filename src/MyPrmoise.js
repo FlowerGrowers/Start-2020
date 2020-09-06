77838bcb701081012e4913662ab841b8877f51a3
nanshou <1007274033@qq.com>
2020-09-06 23:50:26 +0800

    Updated 链式调用的实现

--- src/MyPrmoise.js
+++ src/MyPrmoise.js
@@ -1,7 +1,7 @@
 /*
  * @Author: jack
  * @Date: 2020-09-06 17:18:23
- * @LastEditTime: 2020-09-06 19:45:26
+ * @LastEditTime: 2020-09-06 23:48:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \手写Promise\src\MyPrmoise.js
@@ -10,6 +10,10 @@
 const PENDING = "PENDING",
   FULFILLED = "FULFILLED",
   REJECTED = "REJECTED";
+
+const reslovePromise = (promise2, x, resolve, reject) => {
+  console.log(x);
+};
 
 class MyPromise {
   constructor(executor) {
@@ -47,24 +51,52 @@ class MyPromise {
   }
 
   then(onFulfilled, onRejected) {
-    if (this.status === FULFILLED) {
-      onFulfilled(this.value);
-    }
+    let promise2 = new MyPromise((resolve, reject) => {
+      // 不论是在onFulfilled中还是在onRejected中 抛出异常时总是 reject(error)
+      if (this.status === FULFILLED) {
+        setTimeout(() => {
+          try {
+            let x = onFulfilled(this.value);
+            reslovePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
 
-    if (this.status === REJECTED) {
-      onRejected(this.reason);
-    }
+      if (this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason);
+            reslovePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
 
-    // 发布订阅的模式
-    if (this.status === PENDING) {
-      this.onFulfilledCallBacks.push(() => {
-        onFulfilled(this.value);
-      });
+      // 发布订阅的模式
+      if (this.status === PENDING) {
+        this.onFulfilledCallBacks.push(() => {
+          try {
+            let x = onFulfilled(this.value);
+            reslovePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
 
-      this.onRejectedCallBacks.push(() => {
-        onRejected(this.reason);
-      });
-    }
+        this.onRejectedCallBacks.push(() => {
+          try {
+            let x = onRejected(this.reason);
+            reslovePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
+    });
+    return promise2;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
 /*
  * @Author: your name
  * @Date: 2020-09-06 17:18:50
- * @LastEditTime: 2020-09-06 19:45:00
+ * @LastEditTime: 2020-09-06 23:48:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edi
  * @FilePath: \手写Promise\src\index.js
  */
 const MyPromise = require("./MyPrmoise");
 
+// const promise = new MyPromise((resolve, reject) => {
+//   // resolve('success');
+//   // reject('faile');
+//   // throw new Error('Exception:Error!!!');
+//   setTimeout(() => {
+//     resolve("success");
+//   }, 2000);
+// });
+
+// promise.then(
+//   (value) => {
+//     console.log(value);
+//   },
+//   (reason) => {
+//     console.log(reason);
+//   }
+// );
+
 const promise = new MyPromise((resolve, reject) => {
-  // resolve('success');
-  // reject('faile');
-  // throw new Error('Exception:Error!!!');
-  setTimeout(() => {
-    resolve("success");
-  }, 2000);
+  resolve("success");
 });
 
-promise.then(
-  (value) => {
+const promise2 = promise
+  .then((value) => {
+    return value + " " + "-> then" + " " + "promise2";
+  })
+  .then((value) => {
     console.log(value);
-  },
-  (reason) => {
-    console.log(reason);
-  }
-);
+  });
