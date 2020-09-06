0391a37e0d2131e16548947543c05cc01e39a44a
nanshou <1007274033@qq.com>
2020-09-06 19:43:03 +0800

    Updated Promise处理异步函数 使用发布订阅的模式

--- src/MyPrmoise.js
+++ src/MyPrmoise.js
@@ -1,7 +1,7 @@
 /*
  * @Author: jack 
  * @Date: 2020-09-06 17:18:23
- * @LastEditTime: 2020-09-06 18:31:01
+ * @LastEditTime: 2020-09-06 19:42:02
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \手写Promise\src\MyPrmoise.js
@@ -17,28 +17,33 @@ class MyPromise {
     this.value = undefined;
     this.reason = undefined;
 
+    // 需要两个容器存放异步执行的函数
+
+    this.onFulfilledCallBacks = [];
+    this.onRejectedCallBacks = [];
 
     const resolve = (value) => {
       if (this.status === PENDING) {
         this.value = value;
         this.status = FULFILLED;
+
+        // 发布
+        this.onFulfilledCallBacks.forEach(fn => fn());
       }
     }
-
     const reject = (reason) => {
       if (this.status === PENDING) {
         this.reason = reason;
         this.status = REJECTED;
+
+        this.onRejectedCallBacks.forEach(fn => fn());
       }
     }
-
     try {
       executor(resolve, reject);
     } catch (error) {
       reject(error);
     }
-
-
   }
 
   then(onFulfilled, onRejected) {
@@ -48,6 +53,17 @@ class MyPromise {
 
     if (this.status === REJECTED) {
       onRejected(this.reason)
+    }
+
+    // 发布订阅的模式
+    if (this.status === PENDING) {
+      this.onFulfilledCallBacks.push(() => {
+        onFulfilled(this.value);
+      })
+
+      this.onRejectedCallBacks.push(() => {
+        onRejected(this.reason)
+      })
     }
 
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-06 17:18:50
+ * @LastEditTime: 2020-09-06 19:41:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edi
+ * @FilePath: \手写Promise\src\index.js
+ */
 const MyPromise = require('./MyPrmoise');
 
 const promise = new MyPromise((resolve, reject) => {
   // resolve('success');
   // reject('faile');
-  throw new Error('Exception:Error!!!');
+  // throw new Error('Exception:Error!!!');
+  setTimeout(() => {
+    resolve('success')
+  }, 2000)
 })
 
 promise.then((value) => {
