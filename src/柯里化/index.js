35c2184548a86ce8f11b2f159029eefac0fd96ac
jiajunda <1007274033@qq.com>
2021-03-17 09:50:33 +0800

    封装一个好用的科里函数的包装器

--- src/柯里化/index.js
+++ src/柯里化/index.js
@@ -1,12 +1,18 @@
 // func 是要转换的函数
-function curried(...args) {
-  if (args.length >= func.length) {
-    // (1)
-    return func.apply(this, args);
-  } else {
-    return function pass(...args2) {
-      // (2)
-      return curried.apply(this, args.concat(args2));
-    };
-  }
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
 }
+
+function getSomething(...args) {
+  // console.log(a);
+}
+
+console.log(getSomething.length);
--- src/柯里化/面试题/01.实现.js
+++ src/柯里化/面试题/01.实现.js
@@ -1,13 +1,34 @@
 // 实现add(1)(2)(3) => add(1,2,3);
-function curriedAdd(a) {
-  return function(b) {
-    b += a;
-    return function(c) {
-      return (c += b);
-    };
+// function curriedAdd(a) {
+//   return function(b) {
+//     b += a;
+//     return function(c) {
+//       return (c += b);
+//     };
+//   };
+// }
+
+// 实现add(1,2,3) => add(1,2)(3) =>  add(1)(2)(3)
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function(...args2) {
+        // 本质上就是拼接参数
+        return curried.apply(this, args.concat(args2));
+      };
+    }
   };
 }
 
-const result = curriedAdd(100)(200)(3);
+// const result = curriedAdd(100)(200)(3);
 
-console.log(result);
+function add(x, y, z) {
+  return x + y + z;
+}
+
+const curriedAdd = curry(add);
+
+console.log(curriedAdd(1)(2)(3));
+console.log(curriedAdd(1, 2)(3));
